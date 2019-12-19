e2b3c0172f75e8e2dfca98ce19fd9626822f2023
Noah C. Benson <nben@nyu.edu>
2019-12-19 09:24:04 -0500

    added comments about next steps

--- s4_prf.m
+++ s4_prf.m
@@ -38,3 +38,18 @@ bidsAnalyzePRF(projectDir, subject, session, tasks, runnums, ...
 % run the full PRF analysis (optimze each voxel, can be many hours)
 bidsAnalyzePRF(projectDir, subject, session, tasks, runnums, ...
        dataFolder, dataStr, apertureFolder, modelTypeFine, prfOptsPathFine, tr)
+
+%% 
+% Convert the outputs into MGZ format for use in step 6 (atlases).
+% To do this:
+%   1. read in the output ang, ecc, R2, and rfsize files
+%   2. get the subject's FreeSurfer directory and read in surf/lh.curv using read_curv()
+%   3. lh_vertex_count = numel(lh_curv); rh_vertex_count = numel(ang) - lh_vertex_count;
+%   4. lh_ang = ang(1:lh_vertex_count); rh_ang = ang(lh_vertex_count+1:end);
+%   5. the angle needs to be converted to degrees clockwise (with 0 as the UVM), so probably:
+%       fixed_ang = mod(90 - ang + 180, 360) - 180
+%      (I think aprf outputs angle in degrees ccw starting at the RHM)
+%   6. ecc and rfsize need to be converted from px to deg; R2 needs to be converted from % to fraction (R2/100)
+%   7. Use MRIwrite() (from matlab FreeSurfer toolbox) to write out mgz files. To get an example header, you
+%      can read in any of the mgz files in the subject's directory then replace the mgz.vol field with the
+%      vertex data; the header doesn't matter for vertex data.
