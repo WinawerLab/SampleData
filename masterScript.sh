1b04baaa3b3ea6a771f0fa994b127738d9425d3e
noahbenson <nben@nyu.edu>
2020-02-10 16:02:00 -0500

    new checks

--- masterScript.sh
+++ masterScript.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
-[ -r /Applications/freesurfer/license.txt ] || {
-    echo "No freesurfer license found!"
+function die {
+    echo "$*"
     exit 1
 }
+
+
+
+# Sanity checks:
+[ -z "`which matlab`" ] && die "matlab is not on the path!"
+[ -r /Applications/freesurfer/license.txt ] || die "No freesurfer license found!"
+
+
 
 mkdir temp && cd temp
 git clone https://github.com/winawerlab/sampledata && cd sampledata
