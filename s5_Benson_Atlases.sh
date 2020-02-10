fb7d4f506bc466a609eabef1ee8d7810066ef209
Noah C. Benson <nben@nyu.edu>
2020-02-10 15:28:30 -0500

    Removed the ses folder.

--- s5_Benson_Atlases.sh
+++ s5_Benson_Atlases.sh
@@ -12,28 +12,28 @@ ses=01
 # aprf_run is used by the bayesian inference
 aprf_run=coarse 
 
-mkdir -p "$(pwd)/BIDS/derivatives/atlases/sub-${sub}/ses-${ses}"
+mkdir -p "$(pwd)/BIDS/derivatives/atlases/sub-${sub}"
 
 docker run --rm -it \
            -v "$(pwd)/BIDS/derivatives/freesurfer:/subjects/" \
            -v "$(pwd)/BIDS:/bids" \
            nben/neuropythy atlas --verbose "sub-${sub}" \
-               --output-path="/bids/derivatives/atlases/sub-${sub}/ses-${ses}"
+               --output-path="/bids/derivatives/atlases/sub-${sub}"
 
 docker run --rm -it \
            -v "$(pwd)/BIDS/derivatives/freesurfer:/subjects" \
            -v "$(pwd)/BIDS:/bids" \
            nben/neuropythy register_retinotopy "sub-${sub}" --verbose --max-input-eccen=12 \
-               --surf-outdir="/bids/derivatives/atlases/sub-${sub}/ses-${ses}" \
-               --vol-outdir="/bids/derivatives/atlases/sub-${sub}/ses-${ses}" \
-               --lh-theta=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/ses-${ses}/lh.angle.mgz \
-               --rh-theta=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/ses-${ses}/rh.angle.mgz \
-               --lh-eccen=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/ses-${ses}/lh.eccen.mgz \
-               --rh-eccen=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/ses-${ses}/rh.eccen.mgz \
-               --lh-radius=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/ses-${ses}/lh.sigma.mgz \
-               --rh-radius=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/ses-${ses}/rh.sigma.mgz \
-               --lh-weight=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/ses-${ses}/lh.vexpl.mgz \
-               --rh-weight=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/ses-${ses}/rh.vexpl.mgz
+               --surf-outdir="/bids/derivatives/atlases/sub-${sub}" \
+               --vol-outdir="/bids/derivatives/atlases/sub-${sub}" \
+               --lh-theta=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/lh.angle.mgz \
+               --rh-theta=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/rh.angle.mgz \
+               --lh-eccen=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/lh.eccen.mgz \
+               --rh-eccen=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/rh.eccen.mgz \
+               --lh-radius=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/lh.sigma.mgz \
+               --rh-radius=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/rh.sigma.mgz \
+               --lh-weight=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/lh.vexpl.mgz \
+               --rh-weight=/bids/derivatives/analyzePRF/${aprf_run}/sub-${sub}/rh.vexpl.mgz
 
 
 
