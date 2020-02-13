59ee839b99b91fb31a58222de49b336d1a4ddbe6
jwinawer <jonathan.winawer@nyu.edu>
2020-02-13 10:32:29 -0800

    die if errors in masterscript

--- masterScript.sh
+++ masterScript.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Exit upon any error
+set -euxo pipefail
+
 function die {
     echo "$*"
     exit 1
@@ -11,14 +14,14 @@ function die {
 
 ./s0_download-data.sh "$PWD/DownloadedData" && cd DownloadedData
 
-./s1_preprocess-data.sh
+../s1_preprocess-data.sh
 
-./s2_addToBIDS.sh
+../s2_addToBIDS.sh
 
-matlab -nodisplay -nodesktop -nosplash ./s3_glmDenoise.m
+matlab -nodisplay -nodesktop -nosplash ../s3_glmDenoise.m
 
-matlab -nodisplay -nodesktop -nosplash ./s4_prf.m
+matlab -nodisplay -nodesktop -nosplash ../s4_prf.m
 
-./s5_Benson_Atlases.sh
+../s5_Benson_Atlases.sh
 
 
