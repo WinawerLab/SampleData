fe99edde32d3610bed57025558edde11fd706871
jwinawer <jonathan.winawer@nyu.edu>
2020-02-13 10:12:05 -0800

    minor tweaks

--- masterScript.sh
+++ masterScript.sh
@@ -9,11 +9,6 @@ function die {
 [ -z "`which matlab`" ] && die "matlab is not on the path!"
 [ -r /Applications/freesurfer/license.txt ] || die "No freesurfer license found!"
 
-mkdir temp && cd temp
-git clone https://github.com/winawerlab/sampledata && cd sampledata
-
-mkdir -p DownloadedData 
-
 ./s0_download-data.sh "$PWD/DownloadedData" && cd DownloadedData
 
 ./s1_preprocess-data.sh
