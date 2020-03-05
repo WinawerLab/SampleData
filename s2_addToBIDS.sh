79bc1702bf1c0c7b4a2c5f1cff934cd7159ddb2c
jwinawer <jonathan.winawer@nyu.edu>
2020-03-05 11:10:01 -0500

    add call to setup.sh

--- s2_addToBIDS.sh
+++ s2_addToBIDS.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 #add in the stimfiles and other extras
+
+# Get the path to the sample data, defined as SAMPLE_DATA_DIR
+source setup.sh
+
+# COPY_DISABLE=1 to not copy the .DS_Store
 export COPYFILE_DISABLE=1
-rsync -avzh ./AddThisToBIDSFolder/  ./BIDS/
+
+rsync -avzh "$SAMPLE_DATA_DIR/AddThisToBIDSFolder/"  "$SAMPLE_DATA_DIR/BIDS/"
