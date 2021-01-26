d08e3ae114e8e3ee4635f6f84245cd25e8ba2a2a
Jan Kurzawski <jk7127@deathvalley.psych.nyu.edu>
2021-01-26 13:47:23 -0500

    s0 script now checks if data has been downloaded and unzipped

--- s0_download-data.sh
+++ s0_download-data.sh
@@ -7,10 +7,34 @@ set -euxo pipefail
 
 source setup.sh
 
-curl -L --output "$SAMPLE_DATA_DIR/dicoms.zip"  https://osf.io/9up2d/download
-curl -L --output "$SAMPLE_DATA_DIR/AddThisToBIDS.zip" https://osf.io/ea8b2/download
 
-# Unzip
-unzip "$SAMPLE_DATA_DIR/AddThisToBIDS.zip" -d "$SAMPLE_DATA_DIR" && unzip "$SAMPLE_DATA_DIR/dicoms.zip" -d "$SAMPLE_DATA_DIR"
+
+#Downlad
+
+if  [[ -f "$SAMPLE_DATA_DIR/dicoms.zip" && -f "$SAMPLE_DATA_DIR/AddThisToBIDS.zip" ]]; then
+
+    echo "Data will not be downloaded - files already exist"
+
+else
+
+    curl -L --output "$SAMPLE_DATA_DIR/dicoms.zip"  https://osf.io/9up2d/download
+    curl -L --output "$SAMPLE_DATA_DIR/AddThisToBIDS.zip" https://osf.io/ea8b2/download
+fi
+
+
+#Unzip
+
+
+if  [[ -d "$SAMPLE_DATA_DIR/dicoms" && -d "$SAMPLE_DATA_DIR/AddThisToBIDSFolder" ]]; then
+
+
+    echo "Data will not be unzipped - files already exist"
+
+else
+
+
+    unzip "$SAMPLE_DATA_DIR/AddThisToBIDS.zip" -d "$SAMPLE_DATA_DIR" && unzip "$SAMPLE_DATA_DIR/dicoms.zip" -d "$SAMPLE_DATA_DIR"
+
+fi
 
 exit 0
