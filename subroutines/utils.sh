c9f208179f2fe29b6d52f7bc09133467377b3b92
jwinawer <jonathan.winawer@nyu.edu>
2021-03-10 10:57:46 -0500

    incorporating container_run code which in turn calls docker run or singularity run

--- subroutines/utils.sh
+++ subroutines/utils.sh
@@ -6,10 +6,29 @@ function which_software() {
 	fi
 }
 
+function singularity_pull() {
+	umask u=rwx,g=rx,o=rx
+	export SINGULARITY_CACHEDIR=/scratch/work/$USER/.singularity
+	cd ~/singularity
+	source=${1%/*}
+	[ ${#source} -gt 0 ] && mkdir -p $source && cd $source
+	singularity pull docker://${1}
+}
+
 function container_pull() {
 	if [[ $CONTAINER_SOFTWARE == DOCKER ]]; then
 		docker pull $@
+	else		
+		singularity_pull $@
+	fi
+}
+
+
+}
+function container_run() {
+	if [[ $CONTAINER_SOFTWARE == DOCKER ]]; then
+		docker run $@
 	else
-		singularity pull docker://$@
+		singularity run $@
 	fi
 }
