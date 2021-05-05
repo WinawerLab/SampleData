d4aebb66a951faa01e9477a768df7ca07c9ab6eb
Jon Winawer <jonathan.winawer@nyu.edu>
2021-05-05 13:42:25 -0400

    use %u for userid in sbatch script

--- hpc_greene_submit_s0.sh
+++ hpc_greene_submit_s0.sh
@@ -5,9 +5,9 @@
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=12g
 #SBATCH --time=1:00:00
-#SBATCH --output=/scratch/$USER/logs/out.txt # Define output log location
-#SBATCH --error=/scratch/jaw288/logs/err.txt # and the error logs for when it inevitably crashes
-#SBATCH --mail-user=jaw288@nyu.edu
+#SBATCH --output=/scratch/%u/logs/out.txt # Define output log location
+#SBATCH --error=/scratch/%u/logs/err.txt # and the error logs for when it inevitably crashes
+#SBATCH --mail-user=%u@nyu.edu
 #SBATCH --mail-type=END #email me when it crashes or better, ends
 
 ./s0_download-data.sh
