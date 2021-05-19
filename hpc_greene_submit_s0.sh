1f2564e66f7c019902cba2a91a2f6d9b2b7df245
Jonathan Winawer <jonathan.winawer@nyu.edu>
2021-05-19 10:32:07 -0400

    Update hpc_greene_submit_s0.sh

--- hpc_greene_submit_s0.sh
+++ hpc_greene_submit_s0.sh
@@ -2,12 +2,13 @@
 #
 #SBATCH --job-name=sampledata
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=20
-#SBATCH --mem=12g
-#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=20g
+#SBATCH --time=48:00:00
 #SBATCH --output=/scratch/%u/logs/out.txt # Define output log location
 #SBATCH --error=/scratch/%u/logs/err.txt # and the error logs for when it inevitably crashes
 #SBATCH --mail-user=%u@nyu.edu
 #SBATCH --mail-type=END #email me when it crashes or better, ends
 
 ./s0_download-data.sh
+./s1_preprocess-data.sh
