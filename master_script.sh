f127c0a61ce9b00621de432ba56c0fd45f92ad84
Jonathan Winawer <jonathan.winawer@nyu.edu>
2021-09-08 10:18:15 -0400

    Update master_script.sh

--- master_script.sh
+++ master_script.sh
@@ -14,13 +14,8 @@ function die {
 [ -z "`which matlab`" ] && die "matlab is not on the path!"
 #	Check for freesurfer license file
 [ -r $(fsLicensePath) ] || die "No freesurfer license found!"
-#	Check for python environment
-python -c 'import PIL, neuropythy, matplotlib' || die "In command line, run <conda activate winawerlab> prior to masterScript."
 
 # run scripts
 ./s0_download_data.sh ${1-}
 ./s1_preprocess_data.sh ${1-}
 ./s2_add_to_bids.sh ${1-}
-./s3_glm_denoise.sh ${1-}
-./s4_vista_prf.sh ${1-}
-./s5_benson_atlases.sh ${1-}
